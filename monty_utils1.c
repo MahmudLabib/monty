eb8b612efadc2d157f0177eadb0b16011500143e
MahmudLabib <mahmud.labiib@gmail.com>
2023-11-25 10:40:33 +0200

    ss

--- monty_utils1.c
+++ monty_utils1.c
@@ -70,7 +70,7 @@ void monty_swap(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		f_errors(3, line_number);
+		f_errors(7, line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*stack)->next->n;
